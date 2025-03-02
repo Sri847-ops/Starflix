17ad63884d7ab1e456c5bda2ea1d0700dd6b56d2
Sriram V <140042614+Sri847-ops@users.noreply.github.com>
2025-03-02 13:50:20 +0530

    Created the layout
    - Created the basic structure for the project

---
+++ src/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      {/* Nav */}
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDVDdEpXu61o9l5wtZ2xHuoMvP5shQnR-I",
+    authDomain: "starflix-9e4be.firebaseapp.com",
+    projectId: "starflix-9e4be",
+    storageBucket: "starflix-9e4be.firebasestorage.app",
+    messagingSenderId: "759400808731",
+    appId: "1:759400808731:web:f6cdb97129d94613d453be",
+    measurementId: "G-WPTHWTM40G"
+  };
+
+const dependencies = {
+  "ajv": "^8.0.0",
+  "ajv-keywords": "^5.0.0"
+};
