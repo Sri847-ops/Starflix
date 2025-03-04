8740c9ce7d05108638f1b81cffa4698becb7b67b
Sriram V <140042614+Sri847-ops@users.noreply.github.com>
2025-03-04 13:33:16 +0530

    feat: added the navbar for the home screen
    Added the navbar for the home screen

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Nav from './Nav'
 function HomeScreen() {
   return (
     <div className='homeScreen'>
       {/* Nav */}
-
+      <Nav />
       {/* Banner */}
 
       {/* Row */}
--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,21 @@ import './Nav.css'
 
 function Nav() {
   return (
-    <div className='nav'>
+    <div className='nav nav_black'>
+      <div className='nav_contents'>
+        <img 
+          className="nav_logo"
+          src="https://wallpapers.com/images/hd/netflix-logo-redon-black-l2sdc7dpfo35m127.png"
+          alt="" 
+        />
+
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
+          alt=""  
+        />
+        <br></br>
+      </div>
       
     </div>
   )
